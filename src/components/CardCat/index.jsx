b5178a849622fd273cec9823956ff5d97086c370
Gonzalo Fleming <gonzafg2@gmail.com>
2022-12-07 22:24:46 -0300

    Saco la descripción

--- src/components/CardCat/index.jsx
+++ src/components/CardCat/index.jsx
@@ -29,10 +29,6 @@ const CardCat = () => {
       />
       <div className="card-body">
         <h5 className="card-title">{ poke?.name.toUpperCase() }</h5>
-        <p className="card-text">
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </p>
         <a href="#" className="btn btn-primary">
           Go somewhere
         </a>
